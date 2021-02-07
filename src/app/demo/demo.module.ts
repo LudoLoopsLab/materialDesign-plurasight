7e14c57116e65529c7da3f53041870092c43de5d
Ludo loops <ludoloops@protonmail.com>
2021-02-07 21:29:31 +0100

    Feat: sidenav

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -5,9 +5,17 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material.module';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { FlexboxComponent } from './flexbox/flexbox.component';
 @NgModule({
-  declarations: [ButtonsComponent],
-  imports: [CommonModule, DemoRoutingModule, MaterialModule, FormsModule],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    MaterialModule,
+    FormsModule,
+    FlexLayoutModule,
+  ],
+  declarations: [ButtonsComponent, FlexboxComponent],
 })
 export class DemoModule {}
