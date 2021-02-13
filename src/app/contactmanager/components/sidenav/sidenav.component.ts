9169701de914635dec2f3fd12b0cc0261324c69f
Ludo loops <ludoloops@protonmail.com>
2021-02-13 01:03:26 +0100

    Feat: dark-theme

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -17,6 +17,7 @@ export class SidenavComponent implements OnInit {
   public isScreenSmall: boolean;
 
   users: Observable<User[]>;
+  isDarkTheme: boolean = false;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -25,6 +26,10 @@ export class SidenavComponent implements OnInit {
   ) {}
 
   @ViewChild(MatSidenav) sidenav: MatSidenav;
+
+  toggleTheme(): void {
+    this.isDarkTheme = !this.isDarkTheme;
+  }
 
   ngOnInit(): void {
     this.breakpointObserver
